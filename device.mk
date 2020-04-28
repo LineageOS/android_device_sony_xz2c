d386c38790428864bf2f35ba96887378148477a2
sjllls <2012ji123@gmail.com>
2020-04-28 09:42:17 +0800

    apollo: Import thermal-engine.conf
    * Also move change.cfg from tama-common
    
    Change-Id: I54e0b070c20e79fa1e00854394ccf50275ce3a9c

--- device.mk
+++ device.mk
@@ -56,3 +56,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp_RF.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp_RF.conf
+
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf

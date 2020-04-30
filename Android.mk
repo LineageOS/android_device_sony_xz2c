6e8dba1b6ce9504955557532fa2c9303542da53b
Olivier Karasangabo <olivier@lineageos.org>
2020-04-30 14:08:54 +0200

    xz2c: Initial commit
    * Rename tree as a Lineage apollo tree already exists.
    
    Change-Id: I05fd1a021911159132291652406870f6cf4b3e1c

--- Android.mk
+++ Android.mk
@@ -15,7 +15,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),apollo)
+ifeq ($(TARGET_DEVICE),xz2c)
   subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
   $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 endif

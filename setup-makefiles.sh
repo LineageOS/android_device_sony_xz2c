b9cd95073cef63c48da6a5b3607455f61bd97a52
Michael Bestas <mkbestas@lineageos.org>
2020-12-28 20:43:12 +0200

    xz2c: Switch to standalone extract utils
    Align with templates while we are at it
    
    Change-Id: I3cd279191c7bd3582f1c1f80c588eb26cf9da933

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 #
-# Copyright (C) 2018-2019 The LineageOS Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
 
-# Required!
 export DEVICE=xz2c
 export DEVICE_COMMON=tama-common
 export VENDOR=sony
-
-export DEVICE_BRINGUP_YEAR=2020
 
 "./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"

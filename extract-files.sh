7a9f0eeb5943fe49f753edd5f6b13bb25b36741a
Michael Bestas <mkbestas@lineageos.org>
2020-12-28 20:43:12 +0200

    xz2c: Switch to standalone extract utils
    Align with templates while we are at it
    
    Change-Id: I3cd279191c7bd3582f1c1f80c588eb26cf9da933

--- extract-files.sh
+++ extract-files.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
-# Copyright (C) 2018-2019 The LineageOS Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -13,11 +14,8 @@ fi
 
 set -e
 
-# Required!
 export DEVICE=xz2c
 export DEVICE_COMMON=tama-common
 export VENDOR=sony
-
-export DEVICE_BRINGUP_YEAR=2020
 
 "./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"

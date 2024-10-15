613ba432a518dbcafb538c275553a2c54af71c83
LuK1337 <priv.luk@gmail.com>
2024-10-15 22:12:50 +0200

    xz2c: Switch to py extract utils
    Change-Id: I385df1ce3d67b291c7b6a543cb6581d2fc60b443

---
+++ extract-files.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'vendor/sony/tama-common',
+]
+
+module = ExtractUtilsModule(
+    'xz2c',
+    'sony',
+    namespace_imports=namespace_imports,
+    add_firmware_proprietary_file=True,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(
+        module, 'tama-common', module.vendor
+    )
+    utils.run()
